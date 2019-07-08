d9bbfd2dbe56095cf3db2e05a8cfbb5b42259851
Titenq <titenq@gmail.com>
2019-07-08 17:01:01 -0300

    data binding

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -11,10 +11,36 @@ export class AppComponent {
   title = 'Hello World';
   nomeTabela: string = 'Meses'
   meses: string[] = ['Janeiro', 'Fevereiro', 'Março']
-
+  nome: string = 'João'
   funcionarios: string[]
+  imgUrl: string = 'http://lorempixel.com/400/200'
+  classeAlerta: string
+  estiloAlerta: string
+  textoDigitado: string = ''
+  funcionario: string = ''
 
   constructor(private funcionariosService: FuncionariosService) {
     this.funcionarios = this.funcionariosService.getFuncionarios()
+  }
+
+  soma(num1: number, num2: number): number {
+    return num1 + num2
+  }
+
+  valorAlerta(nomeAlerta: string): void {
+    this.classeAlerta = nomeAlerta
+  }
+
+  valorEstilo(nomeEstilo: string): void {
+    this.estiloAlerta = nomeEstilo
+  }
+
+  mostrarTextoEnquantoDigita(event: KeyboardEvent) {
+    this.textoDigitado = (event.target as HTMLInputElement).value
+  }
+
+  gravarFuncionario() {
+    this.funcionarios.push(this.funcionario)
+    this.funcionario = ''
   }
 }
